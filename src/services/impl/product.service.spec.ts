1ca230cd22c4ceda69b05e8d422a9b334e746dab
Noureddine Akebli <akeblinourdine14@gmail.com>
2025-12-11 17:54:26 +0100

    initial

---
+++ src/services/impl/product.service.spec.ts
@@ -0,0 +1,56 @@
+import {
+	describe, it, expect, beforeEach,
+	afterEach,
+} from 'vitest';
+import {mockDeep, type DeepMockProxy} from 'vitest-mock-extended';
+import {type INotificationService} from '../notifications.port.js';
+import {createDatabaseMock, cleanUp} from '../../utils/test-utils/database-tools.ts.js';
+import {ProductService} from './product.service.js';
+import {products, type Product} from '@/db/schema.js';
+import {type Database} from '@/db/type.js';
+
+describe('ProductService Tests', () => {
+	let notificationServiceMock: DeepMockProxy<INotificationService>;
+	let productService: ProductService;
+	let databaseMock: Database;
+	let databaseName: string;
+
+	beforeEach(async () => {
+		({databaseMock, databaseName} = await createDatabaseMock());
+		notificationServiceMock = mockDeep<INotificationService>();
+		productService = new ProductService({
+			ns: notificationServiceMock,
+			db: databaseMock,
+		});
+	});
+
+	afterEach(async () => cleanUp(databaseName));
+
+	it('should handle delay notification correctly', async () => {
+		// GIVEN
+		const product: Product = {
+			id: 1,
+			leadTime: 15,
+			available: 0,
+			type: 'NORMAL',
+			name: 'RJ45 Cable',
+			expiryDate: null,
+			seasonStartDate: null,
+			seasonEndDate: null,
+		};
+		await databaseMock.insert(products).values(product);
+
+		// WHEN
+		await productService.notifyDelay(product.leadTime, product);
+
+		// THEN
+		expect(product.available).toBe(0);
+		expect(product.leadTime).toBe(15);
+		expect(notificationServiceMock.sendDelayNotification).toHaveBeenCalledWith(product.leadTime, product.name);
+		const result = await databaseMock.query.products.findFirst({
+			where: (product, {eq}) => eq(product.id, product.id),
+		});
+		expect(result).toEqual(product);
+	});
+});
+
